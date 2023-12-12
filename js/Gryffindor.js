c8d85891d0684913cf554f77d87432cbcf05ead3
ScomRaPv <148404550+ScomRaPv@users.noreply.github.com>
2023-12-12 18:33:40 -0500

    3.1
    cambio en el foother

--- js/Gryffindor.js
+++ js/Gryffindor.js
@@ -1,41 +1,80 @@
-fetch('https://hp-api.onrender.com/api/characters')
-    .then(response => response.json())
-    .then(data => {
-       
-        let gryffindorCharacters = data.filter(character => character.house === 'Gryffindor');
+let urlAPI = 'https://hp-api.onrender.com/api/characters'
 
-        let carrusel = document.getElementById("carruselPrincipal");
+const {createApp} = Vue
 
-        for (let i = 0; i < gryffindorCharacters.length; i += 4) {
-            let carruselItem;
-            if (i < 4) {
-                carruselItem = document.createElement("div");
-                carruselItem.classList.add("carousel-item", "active");
-            } else {
-                carruselItem = document.createElement("div");
-                carruselItem.classList.add("carousel-item");
-            }
-            let contenedor = document.createElement("div");
-            contenedor.classList.add("d-flex", "justify-content-around", "p-5", "m-5", "flex-wrap");
+const app = createApp({
 
-            for (let j = i; j < i + 4; j++) {
-                if (gryffindorCharacters[j] != undefined) {
-                    let card = document.createElement("div");
-                    card.classList.add("card", "tamañoCard", "mb-4");
-                    card.innerHTML = `
-                         <img src="${gryffindorCharacters[j].image}" class="card-img" alt="">
-                            <div class="card-body cardCSS">
-                                <h1 class="card-title">${gryffindorCharacters[j].name}</h1>
-                                <p class="card-sub-title">${gryffindorCharacters[j].house}</p>
-                                <p class="card-info">${gryffindorCharacters[j].species}, ${gryffindorCharacters[j].gender}, ${gryffindorCharacters[j].ancestry}</p>
+    data() {
+        return{
+            gryffindorCharacters:{},
+            carrusel:{},
 
-                                <a href="Details.html?id=${gryffindorCharacters[j].id}" class="btn btn-primary m-2">book tour                           
-                                 </div>
-                    `;
-                    contenedor.appendChild(card);
-                }
-            }
-            carruselItem.appendChild(contenedor);
-            carrusel.appendChild(carruselItem);
+
         }
-    })
+    },
+
+    created(){
+        this.traerData(urlAPI)
+    },
+
+    methods:{
+        traerData(url) {
+            fetch(url).then(response => response.json()).then(data => {
+
+
+
+
+
+
+            })
+
+    }},
+
+    computed:{}
+
+
+})
+
+
+
+// fetch(urlAPI)
+//     .then(response => response.json())
+//     .then(data => {
+       
+//         let gryffindorCharacters = data.filter(character => character.house === 'Gryffindor');
+
+//         let carrusel = document.getElementById("carruselPrincipal");
+
+//         for (let i = 0; i < gryffindorCharacters.length; i += 4) {
+//             let carruselItem;
+//             if (i < 4) {
+//                 carruselItem = document.createElement("div");
+//                 carruselItem.classList.add("carousel-item", "active");
+//             } else {
+//                 carruselItem = document.createElement("div");
+//                 carruselItem.classList.add("carousel-item");
+//             }
+//             let contenedor = document.createElement("div");
+//             contenedor.classList.add("d-flex", "justify-content-around", "p-5", "m-5", "flex-wrap");
+
+//             for (let j = i; j < i + 4; j++) {
+//                 if (gryffindorCharacters[j] != undefined) {
+//                     let card = document.createElement("div");
+//                     card.classList.add("card", "tamañoCard", "mb-4");
+//                     card.innerHTML = `
+//                          <img src="${gryffindorCharacters[j].image}" class="card-img" alt="">
+//                             <div class="card-body cardCSS">
+//                                 <h1 class="card-title">${gryffindorCharacters[j].name}</h1>
+//                                 <p class="card-sub-title">${gryffindorCharacters[j].house}</p>
+//                                 <p class="card-info">${gryffindorCharacters[j].species}, ${gryffindorCharacters[j].gender}, ${gryffindorCharacters[j].ancestry}</p>
+
+//                                 <a href="Details.html?id=${gryffindorCharacters[j].id}" class="btn btn-primary m-2">book tour                           
+//                                  </div>
+//                     `;
+//                     contenedor.appendChild(card);
+//                 }
+//             }
+//             carruselItem.appendChild(contenedor);
+//             carrusel.appendChild(carruselItem);
+//         }
+//     })
