809fad3ad5dd819be5124cfd155f3baafa9abdb9
agudelo123 <anderson-agudelo13@hotmail.com>
2023-12-13 13:15:17 -0500

    filtros

--- js/Gryffindor.js
+++ js/Gryffindor.js
@@ -1,41 +1,122 @@
-fetch('https://hp-api.onrender.com/api/characters')
-    .then(response => response.json())
-    .then(data => {
-       
-        let gryffindorCharacters = data.filter(character => character.house === 'Gryffindor');
-
-        let carrusel = document.getElementById("carruselPrincipal");
-
-        for (let i = 0; i < gryffindorCharacters.length; i += 4) {
-            let carruselItem;
-            if (i < 4) {
-                carruselItem = document.createElement("div");
-                carruselItem.classList.add("carousel-item", "active");
-            } else {
-                carruselItem = document.createElement("div");
-                carruselItem.classList.add("carousel-item");
-            }
-            let contenedor = document.createElement("div");
-            contenedor.classList.add("d-flex", "justify-content-around", "p-5", "m-5", "flex-wrap");
-
-            for (let j = i; j < i + 4; j++) {
-                if (gryffindorCharacters[j] != undefined) {
-                    let card = document.createElement("div");
-                    card.classList.add("card", "tamañoCard", "mb-4");
-                    card.innerHTML = `
-                         <img src="${gryffindorCharacters[j].image}" class="card-img" alt="">
-                            <div class="card-body cardCSS">
-                                <h1 class="card-title">${gryffindorCharacters[j].name}</h1>
-                                <p class="card-sub-title">${gryffindorCharacters[j].house}</p>
-                                <p class="card-info">${gryffindorCharacters[j].species}, ${gryffindorCharacters[j].gender}, ${gryffindorCharacters[j].ancestry}</p>
-
-                                <a href="Details.html?id=${gryffindorCharacters[j].id}" class="btn btn-primary m-2">book tour                           
-                                 </div>
-                    `;
-                    contenedor.appendChild(card);
-                }
-            }
-            carruselItem.appendChild(contenedor);
-            carrusel.appendChild(carruselItem);
+let profesorCheckbox;
+let estudianteCheckbox;
+let aliveCheckBox;
+let deceasedCheckBox;
+let busquedaimpunt;
+let favoritosCheckbox;
+let gryffindorCharacters = [];
+document.addEventListener('DOMContentLoaded', (event) => {
+    try {
+        favoritosCheckbox = document.getElementById('favoritos');
+        profesorCheckbox = document.getElementById('profesorCheckbox');
+        estudianteCheckbox = document.getElementById('estudianteCheckbox');
+        aliveCheckBox = document.getElementById('aliveCheckBox');
+        deceasedCheckBox = document.getElementById('deceasedCheckBox');
+        busquedaimpunt = document.getElementById('busquedaimpunt');
+        favoritosCheckbox.addEventListener('change', filterAndUpdateCarousel);
+        profesorCheckbox.addEventListener('change', filterAndUpdateCarousel);
+        estudianteCheckbox.addEventListener('change', filterAndUpdateCarousel);
+        aliveCheckBox.addEventListener('change', filterAndUpdateCarousel);
+        deceasedCheckBox.addEventListener('change', filterAndUpdateCarousel);
+        busquedaimpunt.addEventListener('input', filterAndUpdateCarousel);
+        fetchAndDisplayCharacters();
+    } catch(error) {
+        console.error("Error al inicializar los elementos del DOM o asignar event listeners: ", error);
+    }
+});
+function filterAndUpdateCarousel() {
+    hideError();
+   
+    if (profesorCheckbox.checked && estudianteCheckbox.checked) {
+        showError('Un personaje no puede ser profesor y estudiante al mismo tiempo.');
+        return;
+    }
+    if (aliveCheckBox.checked && deceasedCheckBox.checked) {
+        showError('Un personaje no puede estar vivo y muerto al mismo tiempo.');
+        return;
+    }
+    const isFavorite = favoritosCheckbox.checked;
+    const isProfesor = profesorCheckbox.checked;
+    const isEstudiante = estudianteCheckbox.checked;
+    const isAlive = aliveCheckBox.checked;
+    const isDeceased = deceasedCheckBox.checked;
+    const searchText = busquedaimpunt.value.toLowerCase();
+    let filteredCharacters = gryffindorCharacters.filter(character => {
+        const matchProfesor = !isProfesor || character.hogwartsStaff;
+        const matchEstudiante = !isEstudiante || character.hogwartsStudent;
+        const matchAlive = !isAlive || character.alive;
+        const matchDeceased = !isDeceased || !character.alive;
+        const matchSearchText = !searchText || character.name.toLowerCase().includes(searchText);
+        return matchProfesor && matchEstudiante && matchAlive && matchDeceased && matchSearchText;
+    });
+    if (isFavorite) {
+        filteredCharacters = filteredCharacters.filter(character =>
+            localStorage.getItem(character.name) === 'true'
+        );
+    }
+    displayCharacters(filteredCharacters);
+}
+function fetchAndDisplayCharacters() {
+    fetch('https://hp-api.onrender.com/api/characters')
+        .then(response => response.json())
+        .then(data => {
+            gryffindorCharacters = data.filter(character => character.house === 'Gryffindor');
+            displayCharacters(gryffindorCharacters);
+        });
+}
+function displayCharacters(characters) {
+    let carrusel = document.getElementById("carruselPrincipal");
+    carrusel.innerHTML = '';
+    if (characters.length === 0) {
+        showError('<div class="error-container"><p class="error-message">No se encontraron personajes que coincidan con la búsqueda.</p></div>');
+        return;
+    } 
+    for (let i = 0; i < characters.length; i += 4) {
+        let carruselItem = document.createElement("div");
+        carruselItem.classList.add("carousel-item");
+        if (i === 0) {
+            carruselItem.classList.add("active");
         }
-    })
+        let contenedor = document.createElement("div");
+        contenedor.classList.add("d-flex", "justify-content-around", "p-5", "m-5", "flex-wrap");
+        for (let j = i; j < i + 4; j++) {
+            if (characters[j]) {
+                let isChecked = localStorage.getItem(characters[j].name) === 'true';
+                let card = document.createElement("div");
+                card.classList.add("card", "tamañoCard", "mb-4");
+                card.innerHTML = `
+                  <img src="${characters[j].image}" class="card-img" alt="">
+                  <div class="card-body cardCSS">
+                      <h1 class="card-title">${characters[j].name}</h1>
+                      <p class="card-sub-title">${characters[j].house}</p>
+                      <p class="card-info">${characters[j].species}, ${characters[j].gender}, ${characters[j].ancestry}</p>
+                      <label class="favorite-label">
+                          <input type="checkbox" class="favorite-checkbox" ${isChecked ? 'checked' : ''} data-character-name="${characters[j].name}"> Agregar a Favoritos
+                      </label>
+                      <a href="Details.html?id=${characters[j].id}" class="btn btn-primary m-2">book tour</a>
+                  </div>
+                `;
+                contenedor.appendChild(card);
+            }
+        }
+        carruselItem.appendChild(contenedor);
+        carrusel.appendChild(carruselItem);
+    }
+    // Añadir los event listeners a los checkboxes creados.
+    document.querySelectorAll('.favorite-checkbox').forEach(checkbox => {
+        checkbox.addEventListener('change', (e) => {
+            let characterName = e.target.getAttribute('data-character-name');
+            localStorage.setItem(characterName, e.target.checked);
+        });
+    });
+}
+fetchAndDisplayCharacters();
+function showError(message) {
+    const errorDiv = document.getElementById('error-message');
+    errorDiv.innerHTML = message; // Usar innerHTML para interpretar el tag HTML
+    errorDiv.style.display = 'block'; // Asegurarse que el contenedor del error sea visible
+}
+function hideError() {
+    const errorDiv = document.getElementById('error-message');
+    errorDiv.style.display = 'none';
+}
--- js/Slytherin.js
+++ js/Slytherin.js
@@ -1,40 +1,122 @@
-fetch('https://hp-api.onrender.com/api/characters')
-    .then(response => response.json())
-    .then(data => {
-        let SlytheringCharacters = data.filter(character => character.house === 'Slytherin');
-
-        let carrusel = document.getElementById("carruselPrincipal");
-
-        for (let i = 0; i < SlytheringCharacters.length; i += 4) {
-            let carruselItem;
-            if (i < 4) {
-                carruselItem = document.createElement("div");
-                carruselItem.classList.add("carousel-item", "active");
-            } else {
-                carruselItem = document.createElement("div");
-                carruselItem.classList.add("carousel-item");
-            }
-            let contenedor = document.createElement("div");
-            contenedor.classList.add("d-flex", "justify-content-around", "p-5", "m-5", "flex-wrap");
-
-            for (let j = i; j < i + 4; j++) {
-                if (SlytheringCharacters[j] != undefined) {
-                    let card = document.createElement("div");
-                    card.classList.add("card", "tamañoCard", "mb-4");
-                    card.innerHTML = `
-                         <img src="${SlytheringCharacters[j].image}" class="card-img" alt="">
-                            <div class="card-body cardCSS">
-                                <h1 class="card-title">${SlytheringCharacters[j].name}</h1>
-                                <p class="card-sub-title">${SlytheringCharacters[j].house}</p>
-                                <p class="card-info">${SlytheringCharacters[j].species}, ${SlytheringCharacters[j].gender}, ${SlytheringCharacters[j].ancestry}</p>
-
-                                <a href="Details.html?id=${SlytheringCharacters[j].id}" class="btn btn-primary m-2">book tour</a>
-                            </div>
-                    `;
-                    contenedor.appendChild(card);
-                }
-            }
-            carruselItem.appendChild(contenedor);
-            carrusel.appendChild(carruselItem);
+let profesorCheckbox;
+let estudianteCheckbox;
+let aliveCheckBox;
+let deceasedCheckBox;
+let busquedaimpunt;
+let favoritosCheckbox;
+let slytherinCharacters = [];
+document.addEventListener('DOMContentLoaded', (event) => {
+    try {
+        favoritosCheckbox = document.getElementById('favoritos');
+        profesorCheckbox = document.getElementById('profesorCheckbox');
+        estudianteCheckbox = document.getElementById('estudianteCheckbox');
+        aliveCheckBox = document.getElementById('aliveCheckBox');
+        deceasedCheckBox = document.getElementById('deceasedCheckBox');
+        busquedaimpunt = document.getElementById('busquedaimpunt');
+        
+        favoritosCheckbox.addEventListener('change', filterAndUpdateCarousel);
+        profesorCheckbox.addEventListener('change', filterAndUpdateCarousel);
+        estudianteCheckbox.addEventListener('change', filterAndUpdateCarousel);
+        aliveCheckBox.addEventListener('change', filterAndUpdateCarousel);
+        deceasedCheckBox.addEventListener('change', filterAndUpdateCarousel);
+        busquedaimpunt.addEventListener('input', filterAndUpdateCarousel);
+        fetchAndDisplayCharacters();
+    } catch(error) {
+        console.error("Error al inicializar los elementos del DOM o asignar event listeners: ", error);
+    }
+});
+function filterAndUpdateCarousel() {
+    hideError();
+    if (profesorCheckbox.checked && estudianteCheckbox.checked) {
+        showError('Un personaje no puede ser profesor y estudiante al mismo tiempo.');
+        return;
+    }
+    if (aliveCheckBox.checked && deceasedCheckBox.checked) {
+        showError('Un personaje no puede estar vivo y muerto al mismo tiempo.');
+        return;
+    }
+    const isFavorite = favoritosCheckbox.checked;
+    const isProfesor = profesorCheckbox.checked;
+    const isEstudiante = estudianteCheckbox.checked;
+    const isAlive = aliveCheckBox.checked;
+    const isDeceased = deceasedCheckBox.checked;
+    const searchText = busquedaimpunt.value.toLowerCase();
+    let filteredCharacters = slytherinCharacters.filter(character => {
+        const matchProfesor = !isProfesor || character.hogwartsStaff;
+        const matchEstudiante = !isEstudiante || character.hogwartsStudent;
+        const matchAlive = !isAlive || character.alive;
+        const matchDeceased = !isDeceased || !character.alive;
+        const matchSearchText = !searchText || character.name.toLowerCase().includes(searchText);
+        return matchProfesor && matchEstudiante && matchAlive && matchDeceased && matchSearchText;
+    });
+    if (isFavorite) {
+        filteredCharacters = filteredCharacters.filter(character =>
+            localStorage.getItem(character.name) === 'true'
+        );
+    }
+    displayCharacters(filteredCharacters);
+}
+function fetchAndDisplayCharacters() {
+    fetch('https://hp-api.onrender.com/api/characters')
+        .then(response => response.json())
+        .then(data => {
+            slytherinCharacters = data.filter(character => character.house === 'Slytherin');
+            displayCharacters(slytherinCharacters);
+        });
+}
+function displayCharacters(characters) {
+    let carrusel = document.getElementById("carruselPrincipal");
+    carrusel.innerHTML = '';
+    if (characters.length === 0) {
+        showError('<div class="error-container"><p class="error-message">No se encontraron personajes que coincidan con la búsqueda.</p></div>');
+        return;
+    } 
+    for (let i = 0; i < characters.length; i += 4) {
+        let carruselItem = document.createElement("div");
+        carruselItem.classList.add("carousel-item");
+        if (i === 0) {
+            carruselItem.classList.add("active");
         }
-    })
+        let contenedor = document.createElement("div");
+        contenedor.classList.add("d-flex", "justify-content-around", "p-5", "m-5", "flex-wrap");
+        for (let j = i; j < i + 4; j++) {
+            if (characters[j]) {
+                let isChecked = localStorage.getItem(characters[j].name) === 'true';
+                let card = document.createElement("div");
+                card.classList.add("card", "tamañoCard", "mb-4");
+                card.innerHTML = `
+                  <img src="${characters[j].image}" class="card-img" alt="">
+                  <div class="card-body cardCSS">
+                      <h1 class="card-title">${characters[j].name}</h1>
+                      <p class="card-sub-title">${characters[j].house}</p>
+                      <p class="card-info">${characters[j].species}, ${characters[j].gender}, ${characters[j].ancestry}</p>
+                      <label class="favorite-label">
+                          <input type="checkbox" class="favorite-checkbox" ${isChecked ? 'checked' : ''} data-character-name="${characters[j].name}"> Agregar a Favoritos
+                      </label>
+                      <a href="Details.html?id=${characters[j].id}" class="btn btn-primary m-2">book tour</a>
+                  </div>
+                `;
+                contenedor.appendChild(card);
+            }
+        }
+        carruselItem.appendChild(contenedor);
+        carrusel.appendChild(carruselItem);
+    }
+    // Añadir los event listeners a los checkboxes creados.
+    document.querySelectorAll('.favorite-checkbox').forEach(checkbox => {
+        checkbox.addEventListener('change', (e) => {
+            let characterName = e.target.getAttribute('data-character-name');
+            localStorage.setItem(characterName, e.target.checked);
+        });
+    });
+}
+fetchAndDisplayCharacters();
+function showError(message) {
+    const errorDiv = document.getElementById('error-message');
+    errorDiv.innerHTML = message; // Usar innerHTML para interpretar el tag HTML
+    errorDiv.style.display = 'block'; // Asegurarse que el contenedor del error sea visible
+}
+function hideError() {
+    const errorDiv = document.getElementById('error-message');
+    errorDiv.style.display = 'none';
+}
